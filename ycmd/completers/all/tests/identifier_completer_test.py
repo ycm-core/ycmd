93587b12836edb163414b63d303af5b0e067c19b
Strahinja Val Markovic <val@markovic.io>
2014-08-26 16:25:24 -0700

    Minor test refactoring

--- ycmd/completers/all/tests/identifier_completer_test.py
+++ ycmd/completers/all/tests/identifier_completer_test.py
@@ -22,74 +22,75 @@ from ycmd.completers.all import identifier_completer as ic
 from ycmd.request_wrap import RequestWrap
 from ycmd.tests.test_utils import BuildRequest
 
-def BuildRequestWrap( column_num, contents ):
+def BuildRequestWrap( contents, column_num, line_num = 1 ):
   return RequestWrap( BuildRequest( column_num = column_num,
+                                    line_num = line_num,
                                     contents = contents ) )
 
 
 def GetCursorIdentifier_StartOfLine_test():
-  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 1, 'foo' ) ) )
-  eq_( 'fooBar', ic._GetCursorIdentifier( BuildRequestWrap( 1, 'fooBar' ) ) )
+  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 'foo', 1 ) ) )
+  eq_( 'fooBar', ic._GetCursorIdentifier( BuildRequestWrap( 'fooBar', 1 ) ) )
 
 
 def GetCursorIdentifier_EndOfLine_test():
-  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 3, 'foo' ) ) )
+  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 'foo', 3 ) ) )
 
 
 def GetCursorIdentifier_PastEndOfLine_test():
-  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( 11, 'foo' ) ) )
+  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( 'foo', 11 ) ) )
 
 
 def GetCursorIdentifier_NegativeColumn_test():
-  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( -10, 'foo' ) ) )
+  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 'foo', -10 ) ) )
 
 
 def GetCursorIdentifier_StartOfLine_StopsAtNonIdentifierChar_test():
-  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 1, 'foo(goo)' ) ) )
+  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 'foo(goo)', 1 ) ) )
 
 
 def GetCursorIdentifier_AtNonIdentifier_test():
-  eq_( 'goo', ic._GetCursorIdentifier( BuildRequestWrap( 4, 'foo(goo)' ) ) )
+  eq_( 'goo', ic._GetCursorIdentifier( BuildRequestWrap( 'foo(goo)', 4 ) ) )
 
 
 def GetCursorIdentifier_WalksForwardForIdentifier_test():
-  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( 1, '       foo' ) ) )
+  eq_( 'foo', ic._GetCursorIdentifier( BuildRequestWrap( '       foo', 1 ) ) )
 
 
 def GetCursorIdentifier_FindsNothingForward_test():
-  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( 5, 'foo   ()***()' ) ) )
+  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( 'foo   ()***()', 5 ) ) )
 
 
 def GetCursorIdentifier_SingleCharIdentifier_test():
-  eq_( 'f', ic._GetCursorIdentifier( BuildRequestWrap( 1, '    f    ' ) ) )
+  eq_( 'f', ic._GetCursorIdentifier( BuildRequestWrap( '    f    ', 1 ) ) )
 
 
 def GetCursorIdentifier_StartsInMiddleOfIdentifier_test():
-  eq_( 'foobar', ic._GetCursorIdentifier( BuildRequestWrap( 4, 'foobar' ) ) )
+  eq_( 'foobar', ic._GetCursorIdentifier( BuildRequestWrap( 'foobar', 4 ) ) )
 
 
 def GetCursorIdentifier_LineEmpty_test():
-  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( 12, '' ) ) )
+  eq_( '', ic._GetCursorIdentifier( BuildRequestWrap( '', 12 ) ) )
 
 
 def PreviousIdentifier_Simple_test():
-  eq_( 'foo', ic._PreviousIdentifier( 2, BuildRequestWrap( 4, 'foo' ) ) )
+  eq_( 'foo', ic._PreviousIdentifier( 2, BuildRequestWrap( 'foo', 4 ) ) )
 
 
 def PreviousIdentifier_WhitespaceAfterIdent_test():
-  eq_( 'foo', ic._PreviousIdentifier( 2, BuildRequestWrap( 6, 'foo     ' ) ) )
+  eq_( 'foo', ic._PreviousIdentifier( 2, BuildRequestWrap( 'foo     ', 6 ) ) )
 
 
 def PreviousIdentifier_JunkAfterIdent_test():
   eq_( 'foo',
-       ic._PreviousIdentifier( 2, BuildRequestWrap( 13, 'foo  ;;()**   ' ) ) )
+       ic._PreviousIdentifier( 2, BuildRequestWrap( 'foo  ;;()**   ', 13 ) ) )
 
 
 def PreviousIdentifier_IdentInMiddleOfJunk_test():
   eq_( 'aa',
-       ic._PreviousIdentifier( 2, BuildRequestWrap( 13, 'foo  ;;(aa)**   ' ) ) )
+       ic._PreviousIdentifier( 2, BuildRequestWrap( 'foo  ;;(aa)**   ', 13 ) ) )
 
 
 def PreviousIdentifier_IdentOnPreviousLine_test():
   eq_( 'aa',
-       ic._PreviousIdentifier( 2, BuildRequestWrap( 13, 'foo  ;;(aa)**   ' ) ) )
+       ic._PreviousIdentifier( 2, BuildRequestWrap( 'foo  ;;(aa)**   ', 13 ) ) )
