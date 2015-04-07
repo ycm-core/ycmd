8880affcc5567d4f59aa9309059872bfe6f269e9
Evan Friis <ekfriis@google.com>
2015-04-07 09:54:47 -0700

    Log which gocode binary is found at startup

--- ycmd/completers/go/hook.py
+++ ycmd/completers/go/hook.py
@@ -17,11 +17,18 @@
 # You should have received a copy of the GNU General Public License
 # along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 from ycmd.completers.go.gocode_completer import \
     GoCodeCompleter, FindGoCodeBinary
 
+_logger = logging.getLogger( __name__ )
+
 def GetCompleter( user_options ):
-  if FindGoCodeBinary( user_options ):
+  _logger.info(user_options)
+  binary = FindGoCodeBinary( user_options )
+  if binary:
+    _logger.info("Enabling go completion using %s", binary)
     return GoCodeCompleter( user_options )
+  _logger.info("Could not find gocode binary, no go completion.")
   return None
 
