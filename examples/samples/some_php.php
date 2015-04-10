02671337500a3039216a07c9b7806f5a7b1b10d4
Neo Mofoka <jeteon@gmail.com>
2015-04-10 03:28:47 +0200

    Added a sample PHP file to the examples used for the example client.

---
+++ examples/samples/some_php.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Sample PHP file for testing
+ */
+
+class Example {
+
+	public function __construct() {
+		$this->x = 1;
+		$this->y = 2;
+		$this->z = 3;
+	}
+}
+
+ex = new Example();
+
+// location after the operator is line 19, column 5
+func
+ex->
+
+echo 'testing files';
