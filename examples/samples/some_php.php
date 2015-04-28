9b102748c80a7a2258b4758911a78ef065f1f980
Neo Mofoka <jeteon@gmail.com>
2015-04-28 21:55:34 +0200

    Completed PHP example file and added function definition for testing.

--- examples/samples/some_php.php
+++ examples/samples/some_php.php
@@ -15,7 +15,20 @@ class Example {
 
 $ex = new Example();
 
-// location after the operator is line 19, column 5
-$ex->
+// location after the -> operator is line 19, column 6
+$ex->x = 5;
+
+// location of cursor is line 22, column 12
+$null = is_null($ex->z);
 
 echo 'testing files';
+
+function test1($i) {
+	$b = 0;
+	if ($i > 0) {
+		$b = $i;
+	}
+}
+
+// Middle call is line 34, column 10
+$t = test1(0);
