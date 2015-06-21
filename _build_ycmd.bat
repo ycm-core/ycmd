e409955c049b731543e8815ec7261d36e282d8b1
rexdf <rexdf@vip.qq.com>
2015-06-21 21:00:17 +0800

    fix LLVM_PATH setting

--- _build_ycmd.bat
+++ _build_ycmd.bat
@@ -3,12 +3,12 @@ cd /d %~dp0
 
 SET PYTHON_PATH=C:\Python27
 
-set PATH=C:\Program Files (x86)\CMake\bin;
+SET PATH=C:\Program Files (x86)\CMake\bin;
 
 if not exist ycmd_build mkdir ycmd_build
 cd ycmd_build
 
-LLVM_PATH=%~dp0\llvm_build\Release
+SET LLVM_PATH=%~dp0\llvm_build\Release
 
 cmake -DPYTHON_EXECUTABLE=%PYTHON_PATH%\python.exe  -DPYTHON_INCLUDE_DIRS=%PYTHON_PATH%\include -DPYTHON_LIBRARIES=%PYTHON_PATH%\libs\python27.lib -DPATH_TO_LLVM_ROOT=%LLVM_PATH% -G "Visual Studio 12 2013 Win64" ..\cpp
 
