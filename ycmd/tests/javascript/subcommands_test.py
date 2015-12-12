392a6b68e51b2e2306286fcd0df17958784cc29e
Ben Jackson <puremourning@gmail.com>
2015-12-12 12:06:26 +0000

    Move integration tests to correct directory

--- ycmd/tests/javascript/subcommands_test.py
+++ ycmd/tests/javascript/subcommands_test.py
@@ -1,20 +1,20 @@
 #
 # Copyright (C) 2015 ycmd contributors
 #
-# This file is part of YouCompleteMe.
+# This file is part of ycmd.
 #
-# YouCompleteMe is free software: you can redistribute it and/or modify
+# ycmd is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# YouCompleteMe is distributed in the hope that it will be useful,
+# ycmd is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with YouCompleteMe.  If not, see <http://www.gnu.org/licenses/>.
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
 
 from ycmd.server_utils import SetUpPythonPath
 SetUpPythonPath()
@@ -199,12 +199,12 @@ def SubCommands_TernCompleter_GoToReferences_Works_test():
       'response': httplib.OK,
       'data': contains_inanyorder(
         has_entries( {
-          'filepath': PathToTestFile( 'coollib/cool_objet.js' ),
+          'filepath': PathToTestFile( 'coollib/cool_object.js' ),
           'line_num':  17,
           'column_num': 29,
         } ),
         has_entries( {
-          'filepath': PathToTestFile( 'coollib/cool_objet.js' ),
+          'filepath': PathToTestFile( 'coollib/cool_object.js' ),
           'line_num': 12,
           'column_num': 9,
         } )
