4fed751458b1fae5a16d04a6ba82c47a8a54e9b0
Alistair Bush <alistair.bush@gmail.com>
2016-01-02 01:46:19 +1300

    GetType now returns namespace.type

--- ycmd/tests/cs/subcommands_test.py
+++ ycmd/tests/cs/subcommands_test.py
@@ -295,7 +295,7 @@ class Cs_Subcommands_test( Cs_Handlers_test ):
                                        filepath = filepath )
 
     eq_( {
-      u'message': u"string"
+      u'message': u"System.string"
     }, self._app.post_json( '/run_completer_command', gettype_data ).json )
 
     self._StopOmniSharpServer( filepath )
