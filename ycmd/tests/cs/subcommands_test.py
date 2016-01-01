b7dd750bc2095b1b5a93b9785018a860f88e16a0
Alistair Bush <alistair.bush@gmail.com>
2016-01-02 01:32:35 +1300

    column number is different,  but not 'bad'

--- ycmd/tests/cs/subcommands_test.py
+++ ycmd/tests/cs/subcommands_test.py
@@ -81,7 +81,7 @@ class Cs_Subcommands_test( Cs_Handlers_test ):
     eq_( {
       'filepath': self._PathToTestFile( 'testy', 'GotoTestCase.cs' ),
       'line_num': 30,
-      'column_num': 3
+      'column_num': 15
     }, self._app.post_json( '/run_completer_command', goto_data ).json )
 
     self._StopOmniSharpServer( filepath )
