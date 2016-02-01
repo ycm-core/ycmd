f3d6e9b9569e6fbbba948b0c76dc252887d7f096
Spencer G. Jones <spencer@egovmt.com>
2016-02-01 10:14:36 -0700

    run_test.py completer paremeters changes:
    Combined various --no-X-completer parameters into an single
    "--no-completer X" parameter and add a "--with-completer X" parameter.
    Deprecate the legacy parameters.

--- run_tests.py
+++ run_tests.py
@@ -31,29 +31,73 @@ def RunFlake8():
   ] )
 
 
+completers = {
+  'cfamily': {
+    'build': [ '--clang-completer' ],
+    'test': [ '--exclude-dir=ycmd/tests/clang' ],
+    'aliases': [ 'c', 'cpp', 'c++', 'objc', 'clang', ]
+  },
+  'cs': {
+    'build': [ '--omnisharp-completer' ],
+    'test': [ '--exclude-dir=ycmd/tests/cs' ],
+    'aliases': [ 'omnisharp', 'csharp', 'c#' ]
+  },
+  'javascript': {
+    'build': [ '--tern-completer' ],
+    'test': [ '--exclude-dir=ycmd/tests/javascript' ],
+    'aliases': [ 'js', 'term' ]
+  },
+  'go': {
+    'build': [ '--gocode-completer' ],
+    'test': [ '--exclude-dir=ycmd/tests/go' ],
+    'aliases': [ 'gocode' ]
+  },
+  'rust': {
+    'build': [ '--racer-completer' ],
+    'test': [ '--exclude-dir=ycmd/tests/rust' ],
+    'aliases': [ 'racer', 'racerd', ]
+  },
+  'typescript': {
+    'build': [],
+    'test': [ '--exclude-dir=ycmd/tests/typescript' ],
+    'aliases': []
+  },
+  'python': {
+    'build': [],
+    'test': [ '--exclude-dir=ycmd/tests/python' ],
+    'aliases': [ 'jedi', 'jedihttp', ]
+  },
+}
+
+
+def CompleterType( value ):
+  value = value.lower()
+  if value in completers:
+    return value
+  else:
+    aliases_to_completer = dict( (i,k) for k,v in completers.iteritems()
+                                          for i in v[ 'aliases' ] )
+    if value in aliases_to_completer:
+      return aliases_to_completer[ value ];
+    else:
+      raise argparse.ArgumentTypeError(
+        '{0} is not a valid completer - should be one of {1}'.format(
+          value, completers.keys() ) )
+
+
 def ParseArguments():
   parser = argparse.ArgumentParser()
-  parser.add_argument( '--no-clang-completer', action = 'store_true',
-                       help = 'Do not test C-family '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-omnisharp-completer', action = 'store_true',
-                       help = 'Do not test C# '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-tern-completer', action = 'store_true',
-                       help = 'Do not test Javascript '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-gocode-completer', action = 'store_true',
-                       help = 'Do not test GoCode '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-racer-completer', action = 'store_true',
-                       help = 'Do not test Racer '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-typescript-completer', action = 'store_true',
-                       help = 'Do not test TypeScript '
-                       'semantic completion engine.' )
-  parser.add_argument( '--no-python-completer', action = 'store_true',
-                       help = 'Do not test Jedi'
-                       'semantic completion engine.' )
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument( '--no-clang-completer', action = 'store_true',
+                       help = argparse.SUPPRESS ) # deprecated 
+  group.add_argument( '--no-completers', nargs ='*', type = CompleterType,
+                       help = 'Do not build or test semantic completion '
+                       'engine(s). Valid values: {0}'.format(
+                        completers.keys()) )
+  group.add_argument( '--with-completers', nargs ='*', type = CompleterType,
+                       help = 'Only build or test listed semantic '
+                       'completion engine(s). Valid values: {0}'.format(
+                        completers.keys()) )
   parser.add_argument( '--skip-build', action = 'store_true',
                        help = 'Do not build ycmd before testing.' )
   parser.add_argument( '--msvc', type = int, choices = [ 11, 12, 14 ],
@@ -67,9 +111,22 @@ def ParseArguments():
 
   parsed_args, nosetests_args = parser.parse_known_args()
 
+  with_completers = set( completers.keys() )
+  if parsed_args.with_completers is not None:
+    with_completers = set( parsed_args.with_completers )
+  elif parsed_args.no_completers is not None:
+    with_completers = with_completers.difference( parsed_args.no_completers )
+  elif parsed_args.no_clang_completer:
+    print( 'WARNING: The "--no-clang-completer" flag is deprecated. Please use "--no-completer cfamily" instead.' )
+    with_completers.remove( 'cfamily' )
+
   if 'USE_CLANG_COMPLETER' in os.environ:
-    parsed_args.no_clang_completer = ( os.environ[ 'USE_CLANG_COMPLETER' ]
-                                       == 'false' )
+    if os.environ[ 'USE_CLANG_COMPLETER' ] == 'false':
+      with_completers.remove( 'cfamily' )
+    else:
+      with_completers.add( 'cfamily' )
+
+  parsed_args.with_completers = list( with_completers )
 
   if 'COVERAGE' in os.environ:
     parsed_args.coverage = ( os.environ[ 'COVERAGE' ] == 'true' )
@@ -80,8 +137,6 @@ def ParseArguments():
 def BuildYcmdLibs( args ):
   if not args.skip_build:
     extra_cmake_args = [ '-DUSE_DEV_FLAGS=ON' ]
-    if not args.no_clang_completer:
-      extra_cmake_args.append( '-DUSE_CLANG_COMPLETER=ON' )
 
     os.environ[ 'EXTRA_CMAKE_ARGS' ] = ' '.join(extra_cmake_args)
     os.environ[ 'YCM_TESTRUN' ] = '1'
@@ -91,14 +146,9 @@ def BuildYcmdLibs( args ):
       p.join( DIR_OF_THIS_SCRIPT, 'build.py' ),
     ]
 
-    if not args.no_omnisharp_completer:
-      build_cmd.extend( [ '--omnisharp-completer' ] )
-    if not args.no_tern_completer:
-      build_cmd.extend( [ '--tern-completer' ] )
-    if not args.no_gocode_completer:
-      build_cmd.extend( [ '--gocode-completer' ] )
-    if not args.no_racer_completer:
-      build_cmd.extend( [ '--racer-completer' ] )
+    for key in completers:
+      if key in args.with_completers:
+        build_cmd.extend( completers[ key ][ 'build' ] )
 
     if args.msvc:
       build_cmd.extend( [ '--msvc', str( args.msvc ) ] )
@@ -111,20 +161,10 @@ def BuildYcmdLibs( args ):
 
 def NoseTests( parsed_args, extra_nosetests_args ):
   nosetests_args = [ '-v' ]
-  if parsed_args.no_clang_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/clang' )
-  if parsed_args.no_tern_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/javascript' )
-  if parsed_args.no_gocode_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/go' )
-  if parsed_args.no_racer_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/rust' )
-  if parsed_args.no_typescript_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/typescript' )
-  if parsed_args.no_omnisharp_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/cs' )
-  if parsed_args.no_python_completer:
-    nosetests_args.append( '--exclude-dir=ycmd/tests/python' )
+
+  for key in completers:
+    if key not in parsed_args.with_completers:
+      nosetests_args.extend( completers[ key ][ 'test' ] )
 
   if parsed_args.coverage:
     nosetests_args += [ '--with-coverage', '--cover-package=ycmd' ]
