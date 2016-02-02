5b920507e1e8befb205668b1eae734806471cc40
Manish Goregaokar <manishsmail@gmail.com>
2016-02-03 01:21:36 +0530

    Add test for Go GoTo

---
+++ ycmd/tests/go/subcommands_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2015 ycmd contributors.
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+from go_handlers_test import Go_Handlers_test
+from nose.tools import eq_
+
+
+class Go_Subcommands_test( Go_Handlers_test ):
+
+
+  def _GoTo( self, params ):
+    filepath = self._PathToTestFile( 'goto.go' )
+    contents = open( filepath ).read()
+
+    command = params[ 'command' ]
+    goto_data = self._BuildRequest( completer_target = 'filetype_default',
+                                    command_arguments = [ command ],
+                                    line_num = 8,
+                                    column_num = 8,
+                                    contents = contents,
+                                    filetype = 'go',
+                                    filepath = filepath )
+
+    results = self._app.post_json( '/run_completer_command',
+                                   goto_data )
+
+    eq_( {
+      'line_num': '3', 'column_num': '6', 'filepath': filepath
+    }, results.json )
+
+
+  def GoTo_all_test( self ):
+    tests = [
+      { 'command': 'GoTo' },
+      { 'command': 'GoToDefinition' },
+      { 'command': 'GoToDeclaration' }
+    ]
+
+    for test in tests:
+      yield ( self._GoTo, test )
