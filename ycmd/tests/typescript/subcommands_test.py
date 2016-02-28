a4a09c88e4dd4148d7611514440b29fcc39142f0
Andrea Cedraro <a.cedraro@gmail.com>
2016-02-28 16:35:36 +0100

    Add typescript GoTo failing test

--- ycmd/tests/typescript/subcommands_test.py
+++ ycmd/tests/typescript/subcommands_test.py
@@ -23,7 +23,7 @@ from future import standard_library
 standard_library.install_aliases()
 from builtins import *  # noqa
 
-from hamcrest import assert_that, contains_inanyorder, has_entries
+from hamcrest import assert_that, has_items, has_entries
 from .typescript_handlers_test import Typescript_Handlers_test
 from ycmd.utils import ReadFile
 
@@ -120,7 +120,7 @@ class TypeScript_Subcommands_test( Typescript_Handlers_test ):
 
     gettype_data = self._BuildRequest( completer_target = 'filetype_default',
                                        command_arguments = [ 'GetDoc' ],
-                                       line_num = 31,
+                                       line_num = 32,
                                        column_num = 2,
                                        contents = contents,
                                        filetype = 'typescript',
@@ -155,7 +155,7 @@ class TypeScript_Subcommands_test( Typescript_Handlers_test ):
                                           filetype = 'typescript',
                                           filepath = filepath )
 
-    expected = contains_inanyorder(
+    expected = has_items(
       has_entries( { 'description': 'var bar = new Bar();',
                      'line_num'   : 28,
                      'column_num' : 5 } ),
@@ -192,3 +192,30 @@ class TypeScript_Subcommands_test( Typescript_Handlers_test ):
                    'line_num': 25,
                    'column_num': 3,
                  } ) )
+
+
+  def GoTo_Fail_test( self ):
+    filepath = self._PathToTestFile( 'test.ts' )
+    contents = ReadFile( filepath )
+
+    event_data = self._BuildRequest( filepath = filepath,
+                                      filetype = 'typescript',
+                                      contents = contents,
+                                      event_name = 'BufferVisit' )
+
+    self._app.post_json( '/event_notification', event_data )
+
+    goto_data = self._BuildRequest( completer_target = 'filetype_default',
+                                    command_arguments = [ 'GoToDefinition' ],
+                                    line_num = 30,
+                                    column_num = 6,
+                                    contents = contents,
+                                    filetype = 'typescript',
+                                    filepath = filepath )
+
+    response = self._app.post_json( '/run_completer_command',
+                                    goto_data,
+                                    expect_errors = True ).json
+    assert_that( response,
+                 self._ErrorMatcher( RuntimeError,
+                                     'Could not find definition' ) )
