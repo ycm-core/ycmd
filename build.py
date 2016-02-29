a913d01f9f7b53a0451750393ad62e2f21959e31
Val Markovic <val@markovic.io>
2016-02-28 16:46:36 -0800

    Make Python version checking more robust

--- build.py
+++ build.py
@@ -15,7 +15,7 @@ import shlex
 import errno
 
 PY_MAJOR, PY_MINOR = sys.version_info[ 0 : 2 ]
-if not ( ( PY_MAJOR == 2 and PY_MINOR in [ 6, 7 ] ) or
+if not ( ( PY_MAJOR == 2 and PY_MINOR >= 6 ) or
          ( PY_MAJOR == 3 and PY_MINOR >= 3 ) or
          PY_MAJOR > 3 ):
   sys.exit( 'ycmd requires Python 2.6, 2.7 or >= 3.3; '
