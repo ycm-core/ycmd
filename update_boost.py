9f920d6104a73d98ef86b4f221a2cdb9010b01b3
Boris Staletic <boris.staletic@gmail.com>
2017-04-20 12:45:40 +0200

    Revert to the old sourceforge url

--- update_boost.py
+++ update_boost.py
@@ -29,8 +29,8 @@ import requests
 CHUNK_SIZE = 1024 * 1024 # 1 MB
 
 BOOST_VERSION_REGEX = re.compile( 'Version (\d+\.\d+\.\d+)' )
-BOOST_URL = ( 'https://dl.bintray.com/boostorg/release/'
-              '{version}/source/{archive}' )
+BOOST_URL = ( 'https://sourceforge.net/projects/boost/files/boost/'
+                      '{version}/{archive}/download' )
 BOOST_NAME = 'boost_{version_}'
 BOOST_ARCHIVE = BOOST_NAME + '.tar.bz2'
 BOOST_PARTS = [
