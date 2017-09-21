cfed48d23d294bd681aaa1452e71b8bf9e775b5e
Ben Jackson <puremourning@gmail.com>
2017-09-21 22:49:10 +0100

    Fix printing of raw messages on py2 and py3
    Print the bytes of the messages rather than throw an exception.
    Ref: https://www.simonmweber.com/2014/11/24/python-logging-traps.html

--- ycmd/completers/language_server/language_server_completer.py
+++ ycmd/completers/language_server/language_server_completer.py
@@ -229,7 +229,7 @@ class LanguageServerConnection( threading.Thread ):
       assert request_id not in self._responses
       self._responses[ request_id ] = response
 
-    _logger.debug( 'TX: Sending message {0}'.format( message ) )
+    _logger.debug( 'TX: Sending message: %r', message )
 
     self._Write( message )
     return response
@@ -241,7 +241,7 @@ class LanguageServerConnection( threading.Thread ):
 
 
   def SendNotification( self, message ):
-    _logger.debug( 'TX: Sending Notification {0}'.format( message ) )
+    _logger.debug( 'TX: Sending notification: %r', message )
 
     self._Write( message )
 
@@ -323,6 +323,8 @@ class LanguageServerConnection( threading.Thread ):
         content_read += len( content )
         read_bytes = content_to_read
 
+      _logger.debug( 'RX: Received message: %r', content )
+
       # lsapi will convert content to unicode
       self._DespatchMessage( lsapi.Parse( content ) )
 
@@ -332,7 +334,6 @@ class LanguageServerConnection( threading.Thread ):
 
 
   def _DespatchMessage( self, message ):
-    _logger.debug( 'RX: Received message: {0}'.format( message ) )
     if 'id' in message:
       with self._responseMutex:
         assert str( message[ 'id' ] ) in self._responses
