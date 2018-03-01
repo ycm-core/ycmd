d4022765f3d1aaf0c9b5214608d1718b4ba64e3c
Boris Staletic <boris.staletic@gmail.com>
2018-03-01 18:59:49 +0100

    Binding tests

---
+++ ycmd/tests/bindings/__init__.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2018 ycmd contributors
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+
+import os.path
+
+
+def PathToTestFile( *args ):
+  dir_of_current_script = os.path.dirname( os.path.abspath( __file__ ) )
+  return os.path.join( dir_of_current_script, 'testdata', *args )
---
+++ ycmd/tests/bindings/testdata/foo.c
@@ -0,0 +1,13 @@
+/// Foo
+int foooo(){
+  int aaaaaaaaa
+  return aaaaaaaaa;
+}
+
+int main(int argc, char *argv[])
+{
+  int bar = foooo();
+  struct { int a; float b; } a;
+  a.b = 5;
+  return 0;
+}
