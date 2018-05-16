b4071216c350dce56b52baa8018e498b6d3fb3cc
micbou <contact@micbou.com>
2018-05-16 20:30:56 +0200

    Remap GoTo commands in Python completer
    Map GoTo, GoToDefinition, and GoToDeclaration to Jedi goto_assignments.
    Add GoToType command and map it to Jedi goto_definitions.

---
+++ ycmd/tests/python/testdata/goto/multifile1.py
@@ -0,0 +1,6 @@
+from multifile3 import my_function
+from multifile2 import my_integer, my_function_alias
+
+my_integer
+my_function
+my_function_alias
---
+++ ycmd/tests/python/testdata/goto/multifile2.py
@@ -0,0 +1,1 @@
+from multifile3 import my_integer, my_function as my_function_alias
