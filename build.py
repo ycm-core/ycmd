fa3490414866ae35e026b0991dba8bd21079432d
Sylvestre Ledru <sylvestre@mozilla.com>
2018-07-23 10:06:48 +0200

    In case of error, show which directory doesn't exist

--- build.py
+++ build.py
@@ -38,8 +38,8 @@ for folder in os.listdir( DIR_OF_THIRD_PARTY ):
   abs_folder_path = p.join( DIR_OF_THIRD_PARTY, folder )
   if p.isdir( abs_folder_path ) and not os.listdir( abs_folder_path ):
     sys.exit(
-      'ERROR: some folders in {0} are empty; you probably forgot to run:\n'
-      '\tgit submodule update --init --recursive\n'.format(
+      'ERROR: some folders {0} in {1} are empty; you probably forgot to run:\n'
+      '\tgit submodule update --init --recursive\n'.format(abs_folder_path,
         DIR_OF_THIRD_PARTY )
     )
 
