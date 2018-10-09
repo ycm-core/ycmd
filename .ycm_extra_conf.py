dcf686cb0e7fe0127a556c08c371a1fdbd227788
micbou <contact@micbou.com>
2018-10-10 01:05:26 +0200

    Only add the necessary directories to Python path

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -30,12 +30,12 @@
 
 from distutils.sysconfig import get_python_inc
 import platform
-import os
+import os.path as p
 import subprocess
 import ycm_core
 
-DIR_OF_THIS_SCRIPT = os.path.abspath( os.path.dirname( __file__ ) )
-DIR_OF_THIRD_PARTY = os.path.join( DIR_OF_THIS_SCRIPT, 'third_party' )
+DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
+DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
 SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
 
 # These are the compilation flags that will be used in case there's no
@@ -104,30 +104,30 @@ if platform.system() != 'Windows':
 # 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
-if os.path.exists( compilation_database_folder ):
+if p.exists( compilation_database_folder ):
   database = ycm_core.CompilationDatabase( compilation_database_folder )
 else:
   database = None
 
 
 def IsHeaderFile( filename ):
-  extension = os.path.splitext( filename )[ 1 ]
+  extension = p.splitext( filename )[ 1 ]
   return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
 
 
 def FindCorrespondingSourceFile( filename ):
   if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
+    basename = p.splitext( filename )[ 0 ]
     for extension in SOURCE_EXTENSIONS:
       replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
+      if p.exists( replacement_file ):
         return replacement_file
   return filename
 
 
 def PathToPythonUsedDuringBuild():
   try:
-    filepath = os.path.join( DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING' )
+    filepath = p.join( DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING' )
     with open( filepath ) as f:
       return f.read().strip()
   # We need to check for IOError for Python 2 and OSError for Python 3.
@@ -186,7 +186,7 @@ def Settings( **kwargs ):
 
 def GetStandardLibraryIndexInSysPath( sys_path ):
   for index, path in enumerate( sys_path ):
-    if os.path.isfile( os.path.join( path, 'os.py' ) ):
+    if p.isfile( p.join( path, 'os.py' ) ):
       return index
   raise RuntimeError( 'Could not find standard library path in Python path.' )
 
@@ -194,23 +194,21 @@ def GetStandardLibraryIndexInSysPath( sys_path ):
 def PythonSysPath( **kwargs ):
   sys_path = kwargs[ 'sys_path' ]
 
-  sys_path.insert( 0, DIR_OF_THIS_SCRIPT )
+  interpreter_path = kwargs[ 'interpreter_path' ]
+  major_version = subprocess.check_output( [
+    interpreter_path, '-c', 'import sys; print( sys.version_info[ 0 ] )' ]
+  ).rstrip().decode( 'utf8' )
 
-  for folder in os.listdir( DIR_OF_THIRD_PARTY ):
-    if folder == 'python-future':
-      folder = os.path.join( folder, 'src' )
-      sys_path.insert( GetStandardLibraryIndexInSysPath( sys_path ) + 1,
-                       os.path.realpath( os.path.join( DIR_OF_THIRD_PARTY,
-                                                       folder ) ) )
-      continue
+  sys_path.insert( GetStandardLibraryIndexInSysPath( sys_path ) + 1,
+                   p.join( DIR_OF_THIRD_PARTY, 'python-future', 'src' ) )
+  sys_path[ 0:0 ] = [ p.join( DIR_OF_THIS_SCRIPT ),
+                      p.join( DIR_OF_THIRD_PARTY, 'bottle' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'cregex',
+                              'regex_{}'.format( major_version ) ),
+                      p.join( DIR_OF_THIRD_PARTY, 'frozendict' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'jedi' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'parso' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'requests' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'waitress' ) ]
 
-    if folder == 'cregex':
-      interpreter_path = kwargs[ 'interpreter_path' ]
-      major_version = subprocess.check_output( [
-        interpreter_path, '-c', 'import sys; print( sys.version_info[ 0 ] )' ]
-      ).rstrip().decode( 'utf8' )
-      folder = os.path.join( folder, 'regex_{}'.format( major_version ) )
-
-    sys_path.insert( 0, os.path.realpath( os.path.join( DIR_OF_THIRD_PARTY,
-                                                        folder ) ) )
   return sys_path
