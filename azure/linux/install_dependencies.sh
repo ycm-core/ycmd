8c4520832e59336163e0ae7b7f41a76a2fed21be
Boris Staletic <boris.staletic@gmail.com>
2019-05-09 22:08:54 +0200

    Update clang-tidy

--- azure/linux/install_dependencies.sh
+++ azure/linux/install_dependencies.sh
@@ -10,6 +10,14 @@ if [ "${YCM_COMPILER}" == "clang" ]; then
   sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
 fi
 
+if [ "${YCM_CLANG_TIDY}" ]; then
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+  sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main"
+  sudo apt-get update
+  sudo apt-get install -y clang-tidy-8
+  sudo update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-8 100
+fi
+
 #
 # Go setup
 #
