14819a7fe80102e9da1a1db01e395ea7ef9106a8
Ben Jackson <puremourning@gmail.com>
2019-06-12 19:20:15 +0100

    Python: implement signature help

--- ycmd/completers/language_server/language_server_completer.py
+++ ycmd/completers/language_server/language_server_completer.py
@@ -1641,7 +1641,7 @@ class LanguageServerCompleter( Completer ):
     return server_trigger_characters
 
 
-  def _GetSignatureTriggerCharacters( self, server_trigger_characters ):
+  def GetSignatureTriggerCharacters( self, server_trigger_characters ):
     """Same as _GetTriggerCharacters but for signature help."""
     return server_trigger_characters
 
@@ -1703,7 +1703,7 @@ class LanguageServerCompleter( Completer ):
                       self.Language(),
                       server_trigger_characters )
 
-        trigger_characters = self._GetSignatureTriggerCharacters(
+        trigger_characters = self.GetSignatureTriggerCharacters(
           server_trigger_characters )
 
         if trigger_characters:
