b341e1f1ddd70a054499a78f4037425a6c16bc7b
Ben Jackson <puremourning@gmail.com>
2019-06-28 22:58:55 +0100

    Java: Implement and test signatiure help

---
+++ ycmd/tests/java/testdata/extra_confs/simple_extra_conf_project/src/SignatureHelp.java
@@ -0,0 +1,20 @@
+
+public class SignatureHelp
+{
+  public SignatureHelp( String signature ) {
+    this.test( "test", "t" );
+    this.test( 1, "2" );
+    char [] data = new char[10];
+    String.copyValueOf( data, 1, 2 );
+    this.unique( 10.0 );
+    String s = new String( data );
+  }
+
+  public void test( String s, String s1 ) {
+    String.copyValueOf( 
+  }
+  public void test( int i, String s ) {
+    SignatureHelp s = new SignatureHelp(
+  }
+  public void unique( double d ) {}
+}
