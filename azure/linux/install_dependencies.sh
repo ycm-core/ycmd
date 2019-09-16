fd0a8cdb4f2cfe3932dd0289479e224f7c7a474e
Ben Jackson <puremourning@gmail.com>
2019-09-16 15:39:46 +0100

    Use openssl 1.0 as python 2.7.1 does not compile with 1.1

--- azure/linux/install_dependencies.sh
+++ azure/linux/install_dependencies.sh
@@ -42,15 +42,18 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/i
 
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
-brew install pyenv
+# We need to use openssl 1.0 because the versions of python we build don't
+# compile with openssl 1.1. We could bump python versions until they do, but
+# we've generally committed to testing older versions.
+brew install openssl@1.0 pyenv
 
 eval "$(pyenv init -)"
 
 # In order to work with ycmd, Python *must* be built as a shared library. This
 # is set via the PYTHON_CONFIGURE_OPTS option.
 PYTHON_CONFIGURE_OPTS="--enable-shared" \
-CFLAGS="-I$(brew --prefix openssl)/include" \
-LDFLAGS="-L$(brew --prefix openssl)/lib" \
+CFLAGS="-I$(brew --prefix openssl@1.0)/include" \
+LDFLAGS="-L$(brew --prefix openssl@1.0)/lib" \
 pyenv install ${YCM_PYTHON_VERSION}
 pyenv global ${YCM_PYTHON_VERSION}
 
