671e8101325c57f820948cc4d17bf7f4ddf98097
Lincoln Bergeson <lbergeson@qualtrics.com>
2019-10-05 16:56:59 -0600

    added support for typescriptreact (.tsx) files

---
+++ ycmd/completers/typescriptreact/hook.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2015 Google Inc.
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+
+from ycmd.completers.typescript.typescript_completer import (
+    ShouldEnableTypeScriptCompleter, TypeScriptCompleter )
+
+
+def GetCompleter( user_options ):
+  if not ShouldEnableTypeScriptCompleter():
+    return None
+
+  return TypeScriptCompleter( user_options )
