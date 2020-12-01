c951acf01ddd0ae68a7b33f703e61ea1661d8ffd
Ilia Choly <ilia.choly@gmail.com>
2020-12-01 13:02:58 -0500

    Update gopls to v0.5.4

--- build.py
+++ build.py
@@ -843,7 +843,7 @@ def EnableGoCompleter( args ):
   new_env[ 'GOPATH' ] = p.join( DIR_OF_THIS_SCRIPT, 'third_party', 'go' )
   new_env.pop( 'GOROOT', None )
   new_env[ 'GOBIN' ] = p.join( new_env[ 'GOPATH' ], 'bin' )
-  CheckCall( [ go, 'get', 'golang.org/x/tools/gopls@v0.5.1' ],
+  CheckCall( [ go, 'get', 'golang.org/x/tools/gopls@v0.5.4' ],
              env = new_env,
              quiet = args.quiet,
              status_message = 'Building gopls for go completion' )
