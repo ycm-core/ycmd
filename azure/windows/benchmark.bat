607fe6f5bc26706712b2b6591c9d459ac3871783
Boris Staletic <boris.staletic@gmail.com>
2021-04-20 08:45:13 +0200

    Switch to github actions

--- azure/windows/benchmark.bat
+++
@@ -1,4 +0,0 @@
-:: Add Python to PATH
-set "PATH=C:\Python;C:\Python\Scripts;%PATH%"
-
-python benchmark.py
--- azure/windows/send_coverage.bat
+++
@@ -1,4 +0,0 @@
-:: Add Python to PATH
-set "PATH=C:\Python;C:\Python\Scripts;%PATH%"
-
-codecov --name "%CODECOV_JOB_NAME%"
