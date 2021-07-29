ed6f057d8a9df033b624b7c057a53420870b2e8b
Anton Romanov <theli.ua@gmail.com>
2021-07-29 14:46:51 -0700

    Add rust-analyzer test testing if proc macro works
    Failure can be triggered by using RUST_TOOLCHAIN = 'nightly-2021-06-22'

---
+++ ycmd/tests/rust/get_completions_proc_macro_test.py
@@ -0,0 +1,75 @@
+# Copyright (C) 2015-2020 ycmd contributors
+#
+# This file is part of ycmd.
+#
+# ycmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ycmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
+
+from hamcrest import ( assert_that,
+                       has_item,
+                       empty,
+                       has_key,
+                       is_not )
+from pprint import pformat
+
+
+from ycmd.tests.rust import PathToTestFile, SharedYcmd
+from ycmd.tests.test_utils import ( BuildRequest,
+                                    CompletionEntryMatcher,
+                                    WithRetry )
+from ycmd.utils import ReadFile
+
+
+@WithRetry
+@SharedYcmd
+def GetCompletions_Basic_test( app ):
+  filepath = PathToTestFile( 'common', 'src', 'macromain.rs' )
+  contents = ReadFile( filepath )
+
+  completion_data = BuildRequest( filepath = filepath,
+                                  filetype = 'rust',
+                                  contents = contents,
+                                  line_num = 33,
+                                  column_num = 14 )
+
+  results = app.post_json( '/completions',
+                           completion_data ).json[ 'completions' ]
+
+  assert_that(
+    results,
+    has_item(
+      CompletionEntryMatcher(
+        'checkpoint'
+      )
+    )
+  )
+
+  # This completer does not require or support resolve
+  assert_that( results[ 0 ], is_not( has_key( 'resolve' ) ) )
+  assert_that( results[ 0 ], is_not( has_key( 'item' ) ) )
+
+  # So (erroneously) resolving an item returns the item
+  completion_data[ 'resolve' ] = 0
+  response = app.post_json( '/resolve_completion', completion_data ).json
+  print( f"Resolve resolve: { pformat( response ) }" )
+
+  # We can't actually check the result because we don't know what completion
+  # resolve ID 0 actually is (could be anything), so we just check that we get 1
+  # result, and that there are no errors.
+  assert_that( response[ 'completion' ], is_not( None ) )
+  assert_that( response[ 'errors' ], empty() )
+
+
+def Dummy_test():
+  # Workaround for https://github.com/pytest-dev/pytest-rerunfailures/issues/51
+  assert True
