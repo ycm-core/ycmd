9c958354ad686bd514055e965484c28cddb079cf
Philipp <philippschlinge@protonmail.com>
2021-10-16 13:29:00 +0200

    windows fix: undeclared identifier ssiz_t

--- cpp/ycm/PythonSupport.cpp
+++ cpp/ycm/PythonSupport.cpp
@@ -15,6 +15,11 @@
 // You should have received a copy of the GNU General Public License
 // along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
 
+#if defined(_MSC_VER)
+    #include <BaseTsd.h>
+    typedef SSIZE_T ssize_t;
+#endif
+
 #include "PythonSupport.h"
 #include "Candidate.h"
 #include "Repository.h"
