380bfd399cfc8999d3fa4fad212056e1ed6e7927
Dominik Kaszewski <dakaszewski@gmail.com>
2023-12-21 20:42:19 +0100

    Explicit index

--- ycmd/tests/cs/debug_info_test.py
+++ ycmd/tests/cs/debug_info_test.py
@@ -209,7 +209,7 @@ class DebugInfoTest( TestCase ):
   def test_GetCompleter_RoslynFromUserOption( self, app, *args ):
     # `@patch` does not play nice with functions defined at class scope
     def _popen_mock( cmdline, **kwargs ):
-      i = cmdline[ 0 ].endswith( 'mono' )
+      i = 1 if cmdline[ 0 ].endswith( 'mono' ) else 0
       assert_that( cmdline[ i ], equal_to( 'my_roslyn.exe' ) )
       # Need to redirect to real binary to allow test to pass
       cmdline[ i ] = PATH_TO_OMNISHARP_ROSLYN_BINARY
