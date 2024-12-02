7cc4ee2cd85bc3eb0703ac4072c0da931d86ba25
Boris Staletic <boris.staletic@protonmail.com>
2024-12-03 00:12:15 +0100

    Upgrade gopls to v0.16.2

--- build.py
+++ build.py
@@ -964,7 +964,7 @@ def EnableGoCompleter( args ):
   new_env.pop( 'GOROOT', None )
   new_env[ 'GOBIN' ] = p.join( new_env[ 'GOPATH' ], 'bin' )
 
-  gopls = 'golang.org/x/tools/gopls@v0.16.1'
+  gopls = 'golang.org/x/tools/gopls@v0.16.2'
   CheckCall( [ go, 'install', gopls ],
              env = new_env,
              quiet = args.quiet,
